dc61c23b37558163738b736850c9e053c77c4230
artiom33 <artiom@local.pc>
2019-04-09 03:01:31 +0100

    Adding flask initialization script.

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+import os
+import sqlite3
+import pandas as pd
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+if not os.path.exists(app.config["DATABASE_NAME"]):
+    conn = sqlite3.connect(app.config["DATABASE_NAME"])
+    # cur = conn.cursor()
+    # cur.execute("CREATE TABLE iris (sepal_length REAL, sepal_width REAL,"
+    #             "petal_length REAL, petal_width REAL, species TEXT);")
+    # conn.commit()
+    iris_df = pd.read_csv(app.config["DATA_URL"])
+    iris_df.to_sql('iris', conn, dtype={
+    'sepal_length':'REAL',
+    'sepal_width':'REAL',
+    'petal_length':'REAL',
+    'petal_width':'REAL',
+	'species':'VARCHAR(255)'})
+
+db = SQLAlchemy(app)
+
+from app import routes
