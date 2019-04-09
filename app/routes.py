2436512b9d2d62341e56f35fdc7c1da99fc38400
artiom33 <artiom@local.pc>
2019-04-09 03:02:22 +0100

    Adding active routes.

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from app import app, db
+
+@app.route('/', defaults={'entries': 5})
+@app.route('/<int:entries>')
+def index(entries):
+    db_query = "select * from iris limit %d" % entries
+    res = db.engine.execute(db_query)
+    return render_template("index.html", users=res.fetchall())
