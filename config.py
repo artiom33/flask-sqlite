44eda101f46e7b267236747fe581b17544d7dd97
artiom33 <artiom@local.pc>
2019-04-09 03:02:55 +0100

    Adding config.

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+database_name = "app.db"
+
+class Config(object):
+    DATABASE_NAME = database_name
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, database_name)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    DATA_URL = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/' \
+               'raw/d546eaee765268bf2f487608c537c05e22e4b221/iris.csv'
+    DATA_HEADERS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
