914501557f17dd05ae2c7d085ed6b679e0e9a9d7
artiom33 <artiom@local.pc>
2019-04-09 03:03:22 +0100

    Adding tests.

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+from app import app
+
+class SimpleRequestTest(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
